a35c1fa99807a073d71332bd9d3324c3a6e0419c
比嘉良知 <hikayoshitomo@hikayoshitomonoMacBook-Pro.local>
2021-05-05 10:53:49 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,12 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    posts = Post.create!(post_params)
+    redirect_to posts
   end
 
   def edit
@@ -18,5 +21,11 @@ class PostsController < ApplicationController
   end
 
   def destory
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
